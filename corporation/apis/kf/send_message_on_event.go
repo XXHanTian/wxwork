1bf61863b3bfb4f7576967d4a39f2ffa6e2b0dfe
Lei <zhonglei000@gmail.com>
2023-02-21 18:04:12 +0800

    add handleer

---
+++ corporation/apis/kf/send_message_on_event.go
@@ -0,0 +1,15 @@
+package kf
+
+import (
+	"bytes"
+	"github.com/fastwego/wxwork/corporation"
+)
+
+const (
+	apiKfSendMessageOnEvent = "/cgi-bin/kf/send_msg"
+)
+
+func SendMessageOnEvent(ctx *corporation.App, payload []byte) ([]byte, error) {
+	data, err := ctx.Client.HTTPPost(apiKfSendMessageOnEvent, bytes.NewReader(payload), "application/json;charset=utf-8")
+	return data, err
+}
